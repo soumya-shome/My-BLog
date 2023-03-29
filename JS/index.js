8391c57d139a2402a45413a5defcf98e04a58671
Soumyadeep Shome <soumyadeepshome99@gmail.com>
2023-03-30 02:11:28 +0530

    30.03.23

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,37 @@
+function onLo(){
+(function(){
+    emailjs.init("Zx6YskytANj24jfjK");
+ })();
+}
+
+
+function navb(){ 
+    const body = document.querySelector("body");
+    const navbar = document.querySelector(".navbar");
+    const menuBtn = document.querySelector(".menu-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
+    const menuB = document.querySelector(".menu-list");
+
+    menuBtn.onclick = ()=>{
+    navbar.classList.add("show");
+    menuBtn.classList.add("hide");
+    body.classList.add("disabled");
+    }
+
+    menuB.onclick = ()=>{
+    navbar.classList.remove("show");
+    menuBtn.classList.remove("hide");
+    body.classList.remove("disabled");
+    }
+
+    cancelBtn.onclick = ()=>{
+    body.classList.remove("disabled");
+    navbar.classList.remove("show");
+    menuBtn.classList.remove("hide");
+    }
+    navbar.classList.add("sticky")
+}
+
 function sendMail(){
     var params = {
         name: document.getElementById("name").value,
